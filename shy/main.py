00ee7c8e10b1e67b40b2a230932702a64cf16cbd
Aman Upadhyay <aman161upadhyay@gmail.com>
2018-02-03 00:03:49 +0530

    Find probable Hydrogen Selenium bonds

--- shy/main.py
+++ shy/main.py
@@ -1,7 +1,14 @@
+import numpy
 import os
 import sys
 
+from shy.atom import Atom
 from shy.protein import Protein
+
+
+class ContinueLabelled(Exception):
+    pass
+
 
 if len(sys.argv) < 2:
     raise RuntimeError("Expected Usage: main.py <path-to-pdb-files>")
@@ -18,6 +25,41 @@ for filename in os.listdir(dir_path):
         protein_data = open(os.path.join(dir_path, filename), 'r')
         proteins.append(Protein(protein_name, protein_data))
 
+count = 0
+probable_hbond = []
+
 for protein in proteins:
-    if not protein.contains_amino_acid("MSE"):
-        print(protein.name+".pdb.hadded.pdb")
+    print(count)
+    count+=1
+    print("Analyzing" + protein.name)
+    se_atoms = protein.get_atoms("MSE", "SE")
+
+    for se in se_atoms:
+        connected_atoms = protein.get_connected_atoms(se)
+        proximal_hydrogen = protein.get_proximal_atoms(se, "H", 2.5)
+
+        for hydrogen in proximal_hydrogen:
+            try:
+                for anti_antecedent in connected_atoms:
+                    bond_angle = Atom.get_bond_angles(se, hydrogen, anti_antecedent)
+                    if bond_angle < numpy.pi/2:
+                        raise ContinueLabelled
+
+            except ContinueLabelled:
+                continue
+
+            proximal_oxygen = protein.get_proximal_atoms(hydrogen, "O", 1)
+            proximal_nitrogen = protein.get_proximal_atoms(hydrogen, "N", 1.05)
+
+            for oxygen in proximal_oxygen:
+                bond_angle = Atom.get_bond_angles(hydrogen, se, oxygen)
+                if bond_angle > numpy.pi / 2:
+                    probable_hbond.append((protein.name, se, hydrogen, oxygen))
+
+            for nitrogen in proximal_nitrogen:
+                bond_angle = Atom.get_bond_angles(hydrogen, se, nitrogen)
+                if bond_angle > numpy.pi / 2:
+                    probable_hbond.append((protein.name, se, hydrogen, nitrogen))
+
+    print(len(probable_hbond))
+    print("--------------------")
